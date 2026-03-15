2ccc0f6612da0fb479654972436637c9229228e1
victornovais-dev <victornovaisdev@gmail.com>
2026-03-15 10:27:56 -0300

    Add: configure app bootstrap and mysql properties

---
+++ src/main/java/com/sistema_contabilidade/SistemaContabilidadeApplication.java
@@ -0,0 +1,12 @@
+package com.sistema_contabilidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaContabilidadeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SistemaContabilidadeApplication.class, args);
+  }
+}
---
+++ src/test/java/com/sistema_contabilidade/SistemaContabilidadeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.sistema_contabilidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaContabilidadeApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
